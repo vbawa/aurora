44900bc3750f9692bbd657871d7a184a262e8c52
Veer <vbawa89@gmail.com>
2018-07-20 21:29:38 -0700

    Replaced inaccurate OpenWeather data with Yahoo Weather

--- weather_light.py
+++ weather_light.py
@@ -7,12 +7,12 @@ import requests
 
 from typing import Tuple
 
-OPENWEATHER_API_KEY = os.environ["OPENWEATHER_API_KEY"]
-OPENWEATHER_CITY_ID = 5391959
-OPENWEATHER_API_URL = "http://api.openweathermap.org"\
-                      "/data/2.5/weather?id={city_id}&appid={api_key}"
+YAHOO_WEATHER_FORECAST_URL = "https://query.yahooapis.com/v1/public/yql?"\
+                             "q=select+item+from+weather.forecast+where+woeid"\
+                             "={woeid}&format=json"
+YAHOO_WEATHER_CITY_ID_SF = 2487956
 
-AURORA = None # Singleton
+AURORA = None  # Lazily-loaded Singleton
 # TODO: Implement discovery.
 AURORA_IP = os.environ["AURORA_IP"]
 AURORA_AUTH_TOKEN = os.environ["AURORA_AUTH_TOKEN"]
@@ -25,23 +25,16 @@ TEMP_MAX_F = 80
 TEMP_MIN_RGB = (0, 0, 200)  # Blue
 TEMP_MAX_RGB = (255, 120, 0) # Orange
 
-def _kelv_to_fahr(deg_k: int) -> int:
-    return 9/5*(deg_k - 273) + 32
-
-def get_today_low_high_fahrenheit(city_id: int) -> Tuple[int, int]:
-    url = OPENWEATHER_API_URL.format(
-            city_id=str(city_id), api_key=OPENWEATHER_API_KEY)
-    resp = requests.get(url)
+def get_today_low_high_fahrenheit(yahoo_city_woeid: int) -> Tuple[int, int]:
+    resp = requests.get(
+            YAHOO_WEATHER_FORECAST_URL.format(woeid=yahoo_city_woeid))
     content_str = resp.content.decode()
     if resp.status_code != 200:
         raise Exception("Openweather API returned status code {} with response "
         "content: {}".format(resp.status_code, content_str))
-
-    return tuple(map(_kelv_to_fahr,
-                     (json.loads(content_str)["main"][k] for k in ("temp_min",
-                                                                   "temp_max"))
-                )
-            )
+    forecast_dct = \
+            json.loads(content_str)["query"]["results"]["channel"]["item"]["forecast"][0]
+    return int(forecast_dct["low"]), int(forecast_dct["high"])
 
 def set_temp_color(pct: int):
     """ Using min and max color RGB tuples as the bound, set the color to
@@ -50,7 +43,7 @@ def set_temp_color(pct: int):
     AURORA = AURORA or nanoleaf.Aurora(AURORA_IP, os.environ["AURORA_AUTH_TOKEN"])
     
     frac = pct/100
-    # Linearly transforms MIN_RGB to MAX_RGB depending on the percentage.
+    # Scales between MIN_RGB and MAX_RGB based on the provided percentage.
     AURORA.rgb = tuple(frac*max_rgb + (1-frac)*min_rgb \
             for min_rgb, max_rgb in zip(TEMP_MIN_RGB, TEMP_MAX_RGB)
     )
@@ -58,7 +51,7 @@ def set_temp_color(pct: int):
 def main():
     # TODO: Once I write the ability to set the panels 50/50, set low and high
     # independently instead of using the avg.
-    avg_temp = sum(get_today_low_high_fahrenheit(OPENWEATHER_CITY_ID))/2
+    avg_temp = sum(get_today_low_high_fahrenheit(YAHOO_WEATHER_CITY_ID_SF))/2
     scaled_avg_temp = (avg_temp - TEMP_MIN_F)/(TEMP_MAX_F - TEMP_MIN_F)
     set_temp_color(scaled_avg_temp*100)
 
